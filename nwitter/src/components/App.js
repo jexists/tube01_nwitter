f32c418daa81a10131037b9cdd8ab568ae0c5e96
jexists <jexists486@gmail.com>
2020-10-02 15:45:42 +0900

    log in account

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 // import { myFirebase } from "myFirebase";
 import { authService } from "myFirebase";
 
 function App() {
-  console.log(authService.currentUser); //null;
+  // console.log(authService.currentUser); //null;
   // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  },[])
+  
+  // setInterval(() => {
+  //   console.log(authService.currentUser);
+  // }, 2000);
+
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..." }
       <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
     </>
   );
