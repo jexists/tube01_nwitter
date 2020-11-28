0ca6e2a67135e241a7bcf8f1ee00eb110c22efbb
jexists <jexists486@gmail.com>
2020-11-28 12:31:43 +0900

    nweet delete

--- nwitter/src/components/Nweet.js
+++ nwitter/src/components/Nweet.js
@@ -1,15 +1,26 @@
+import { dbService } from "myFirebase";
 import React from "react";
 
-const Nweet = ({ nweetObj, isOwner }) => (
-  <div>
-    <h4>{nweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button type="buttton">delete</button>
-        <button type="buttton">edit</button>
-      </>
-    )}
-  </div>
-);
+const Nweet = ({ nweetObj, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm ("정말로 게시글을 삭제하시겠습니까?");
+    console.log(ok);
+    if (ok) {
+      //delete nweet
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+    }
+  }
+  return (
+    <div>
+      <h4>{nweetObj.text}</h4>
+      {isOwner && (
+        <>
+          <button type="buttton" onClick={onDeleteClick}>delete</button>
+          <button type="buttton">edit</button>
+        </>
+      )}
+    </div>
+  )
+};
 
 export default Nweet;
