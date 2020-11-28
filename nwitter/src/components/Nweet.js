69f9f30319d8ae8f773b3a3d55a6b46e9285f736
jexists <jexists486@gmail.com>
2020-11-28 16:38:13 +0900

    delete attachment and text together

--- nwitter/src/components/Nweet.js
+++ nwitter/src/components/Nweet.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { dbService } from "myFirebase";
+import { dbService, storageService } from "myFirebase";
 
 const Nweet = ({ nweetObj, isOwner }) => {
   const [editing, setEditing] = useState(false);
@@ -10,6 +10,7 @@ const Nweet = ({ nweetObj, isOwner }) => {
     if (ok) {
       //delete nweet
       await dbService.doc(`nweets/${nweetObj.id}`).delete();
+      await storageService.refFromURL(nweetObj.attachmentUrl).delete();
     }
   };
   const toggleEditing = () => setEditing((prev) => !prev);
