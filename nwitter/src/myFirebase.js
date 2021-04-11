1d5621db986bdd251b8a3b7b1d2efadbc7e9b987
jexists <jexists486@gmail.com>
2021-04-11 21:41:54 +0900

    environment

--- nwitter/src/myFirebase.js
+++ nwitter/src/myFirebase.js
@@ -2,15 +2,16 @@ import * as firebase from "firebase/app"
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
+import { environment } from "./environments/env"
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBr43TUKFL8rZnmgaE8UcX2EQ-O7QWPIVE",
-  authDomain: "nwitter-ee2a2.firebaseapp.com",
-  databaseURL: "https://nwitter-ee2a2.firebaseio.com",
-  projectId: "nwitter-ee2a2",
-  storageBucket: "nwitter-ee2a2.appspot.com",
-  messagingSenderId: "194553886578",
-  appId: "1:194553886578:web:94e0a5362ecaebc9ca0feb"
+  apiKey: environment.FIREBASE_API_KEY,
+  authDomain: environment.FIREBASE_AUTH_DOMAIN,
+  databaseURL: environment.FIREBASE_DATABASE_URL,
+  projectId: environment.PROJECTID,
+  storageBucket: environment.STORAGE_BUCKET,
+  messagingSenderId: environment.MESSAGINGSENDERID,
+  appId: environment.APPID
 };
 
 // export default firebase.initializeApp(firebaseConfig);
